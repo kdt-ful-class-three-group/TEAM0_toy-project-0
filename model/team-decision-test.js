fa77b16e3dad6c07c69814023b6ad9880d121b3d
kongukjae <kongukjae@gmail.com>
2025-03-14 13:08:42 +0900

    팀 나누기 로직 추가: 섞인 멤버를 홀수 및 짝수 팀으로 나누는 기능 구현

--- model/team-decision-test.js
+++ model/team-decision-test.js
@@ -1,13 +1,36 @@
 import shuffleArray from "./modules/shffleArray.js";
 import demoMembers from "../data/demoMembers.js";
+import { isOdd } from "./modules/odd-or-even-decision.js";  
 
-const temp = "";
-const blueTeam = [];
-const redTeam = [];
+
+
+let temp = "";
+const oddTeam = [];
+const evenTeam = [];
 
 
 
 // demoMembers 배열 섞기
-const shuffledMembers = shuffleArray([...demoMembers]);
+const shuffledResult = shuffleArray([...demoMembers]);
+const shuffledMembers = shuffledResult.shuffledArray;
+
+
+
+
 console.log('섞인 멤버 순서:', shuffledMembers);
 
+// 배열을 순회하면서 팀 나누기
+shuffledMembers.forEach((member, index) => {
+    temp = member;
+    
+    if (isOdd(index)) {
+        oddTeam.push(temp);
+    } else {
+        evenTeam.push(temp);
+    }
+});
+
+
+  console.log('홀수 팀:', oddTeam);
+  console.log('짝수 팀:', evenTeam);
+  
