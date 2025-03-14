7684299ced56cf51237d58de21242a350e7ae9ff
kongukjae <kongukjae@gmail.com>
2025-03-14 14:48:29 +0900

    팀 분배 테스트 기능 개선: distributeTeams 함수 호출 방식 수정 및 테스트 코드 추가

--- model/team-decision-test.js
+++ model/team-decision-test.js
@@ -1,6 +1,5 @@
-import shuffleArray from "./modules/shffleArray.js";
 import demoMembers from "../data/demoMembers.js";
-import { isOdd } from "./modules/odd-or-even-decision.js";
+import distributeTeams from "./modules/distributeTeams.js";
 import TeamMember from "./modules/TeamMember.js";
 
 /**
@@ -9,7 +8,7 @@ import TeamMember from "./modules/TeamMember.js";
  * @version 1.0.0
  *
  * @description
- * 이 모듈은 주어진 멤버 목록을 무작위로 섞어 홀수/짝수 팀으로 나누는 기능을 제공합니다.
+ * 이 모듈은 주어진 멤버 목록을 무작위로 섞어 홀수/짝수 팀으로 나누는 기능을 테스트합니다.
  *
  * @process 동작 순서
  * 1. teamState 객체 초기화
@@ -39,70 +38,30 @@ import TeamMember from "./modules/TeamMember.js";
  * - TeamMember: 멤버 객체 생성자
  */
 
-/**
- * @function distributeTeams
- * @param {Array<string>} members - 분배할 멤버들의 배열
- * @returns {Object} odd와 even 팀으로 분배된 TeamMember 인스턴스들
- * @description
- * 주어진 멤버 배열을 홀수/짝수 인덱스를 기준으로 팀을 분배합니다.
- * 1. 멤버 배열을 무작위로 섞습니다.
- * 2. 섞인 배열을 순회하며 인덱스의 홀짝 여부에 따라 팀을 배정합니다.
- * 3. 각 멤버를 TeamMember 인스턴스로 생성하여 해당 팀에 추가합니다.
- */
-const distributeTeams = (members) => {
-    const teams = {
-        odd: [],
-        even: []
-    };
-
-    // 멤버 배열 섞기
-    const shuffledResult = shuffleArray([...members]);
-    const shuffledMembers = shuffledResult.shuffledArray;
-
-    // 팀 분배
-    shuffledMembers.forEach((member, index) => {
-        if (isOdd(index)) {
-            const memberObj = new TeamMember(member, 'odd', teams.odd.length);
-            teams.odd.push(memberObj);
-        } else {
-            const memberObj = new TeamMember(member, 'even', teams.even.length);
-            teams.even.push(memberObj);
-        }
-    });
-
-    return teams;
-};
-
-// 메인 로직
-const teamState = distributeTeams(demoMembers);
+// 팀 분배 실행
+const { teams, shuffledMembers } = distributeTeams(demoMembers);
 
 // 결과 출력
 console.log('섞인 멤버 순서:', shuffledMembers);
 
 // getInfo() 메서드를 사용하여 팀 현황 출력
 const teamStatus = {
-    odd: teamState.odd.map(member => member.getInfo()),
-    even: teamState.even.map(member => member.getInfo())
+    odd: teams.odd.map(member => member.getInfo()),
+    even: teams.even.map(member => member.getInfo())
 };
 
 console.log('팀 현황:', JSON.stringify(teamStatus, null, 2));
 
-// 멤버 생성
-const member = new TeamMember("피카츄", "odd", 0);
+// 테스트 코드
+const testMember = new TeamMember("피카츄", "odd", 0);
+console.log('테스트 멤버 생성:', testMember.getInfo());
 
-// getter 사용
-console.log(member.memberName); // "피카츄"
-console.log(member.team); // "odd"
+testMember.memberName = "라이츄";
+testMember.team = "even";
+console.log('테스트 멤버 정보 수정:', testMember.getInfo());
 
-// setter 사용
-member.memberName = "라이츄"; // 정상 작동
-member.team = "even"; // ID도 자동으로 업데이트됨
+testMember.assignPM();
+console.log('PM 지정 후:', testMember.getInfo());
 
-// PM 지정/해제
-member.assignPM();
-console.log(member.isPM); // true
-member.unassignPM();
-console.log(member.isPM); // false
-
-// 전체 정보 조회
-console.log(member.getInfo());
+testMember.unassignPM();
+console.log('PM 해제 후:', testMember.getInfo());
