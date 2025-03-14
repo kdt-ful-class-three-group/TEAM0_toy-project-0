696c2c839f87f590fa17c8f04a3630949a1c6aec
kongukjae <kongukjae@gmail.com>
2025-03-14 15:48:10 +0900

    팀 분배 시스템 테스트 개선: TeamDistributor 및 TeamMember 클래스 테스트 케이스 추가 및 콘솔 출력 로직 수정

--- src/tests/team-distribution.test.js
+++ src/tests/team-distribution.test.js
@@ -3,52 +3,68 @@ import TeamDistributor from "../core/team-distributor.js";
 import TeamMember from "../models/TeamMember.js";
 
 /**
- * @fileoverview 팀 멤버 배정 및 관리 시스템 테스트
+ * @fileoverview 팀 분배 시스템 테스트
  * @author Kongukjae
  * @version 1.0.0
  */
 
 /**
  * ===========================
- * 임시 테스트 실행 코드
- * TODO: 추후 Jest 테스트로 마이그레이션 예정
+ * 팀 분배 테스트 케이스
  * ===========================
  */
+console.log('\n------------------------------ 팀 분배 시스템 테스트 ------------------------------');
 
-// 1. 팀 분배 기능 테스트
-console.log('\n[팀 분배 테스트]');
-const { teams, shuffledMembers } = distributeTeams(demoMembers);
-console.log('섞인 멤버 순서:', shuffledMembers);
+// 1. TeamDistributor 인스턴스 생성 및 분배 테스트
+console.log('\n[1. 팀 분배 기능 테스트]');
+const distributor = new TeamDistributor();
+const { teams, shuffledMembers } = distributor.distribute(demoMembers);
 
-// 2. 팀 현황 출력 테스트
-console.log('\n[팀 현황 출력 테스트]');
-const teamStatus = {
-    odd: teams.odd.map(member => member.getInfo()),
-    even: teams.even.map(member => member.getInfo())
-};
-console.log('팀 현황:', JSON.stringify(teamStatus, null, 2));
+console.log('- 섞인 멤버 순서:', shuffledMembers);
+console.log('\n------------------------------ 팀 분배 결과 ------------------------------');
+console.log('- 팀 분배 결과:');
+console.log(JSON.stringify(distributor.getTeamStatus(), null, 2));
+
+console.log('\n------------------------------ TeamMember 클래스 기능 테스트 ------------------------------');
+// 2. TeamMember 클래스 기능 테스트
+console.log('\n[2. TeamMember 기능 테스트]');
+
+// 2.1 기본 생성 및 정보 수정
+const member = new TeamMember("피카츄", "odd", 0);
+console.log('- 멤버 생성:', member.getInfo());
+
+member.memberName = "라이츄";
+member.team = "even";
+console.log('- 정보 수정 후:', member.getInfo());
+
+console.log('\n------------------------------ PM 관련 기능 테스트 ------------------------------');
+// 2.2 PM 관련 기능
+member.assignPM();
+console.log('- PM 지정 후:', member.getInfo());
+
+member.unassignPM();
+console.log('- PM 해제 후:', member.getInfo());
 
 /**
- * TeamMember 클래스 기능 테스트
- * @deprecated 추후 Jest 테스트로 대체 예정
- */
-console.log('\n[TeamMember 클래스 테스트]');
-const testMember = new TeamMember("피카츄", "odd", 0);
-console.log('1. 멤버 생성:', testMember.getInfo());
-
-testMember.memberName = "라이츄";
-testMember.team = "even";
-console.log('2. 정보 수정:', testMember.getInfo());
-
-testMember.assignPM();
-console.log('3. PM 지정:', testMember.getInfo());
-
-testMember.unassignPM();
-console.log('4. PM 해제:', testMember.getInfo());
-
-/**
- * @todo
- * - Jest 테스트 프레임워크 도입
- * - 테스트 케이스 체계화
- * - CI/CD 파이프라인 구축
+ * @todo 추가 필요한 테스트 케이스
+ * 1. 팀 분배 관련
+ * - 홀수 인원일 때의 처리
+ * - 최소/최대 인원 제한 처리
+ * - 중복 멤버 처리
+ * 
+ * 2. 팀원 관리 관련
+ * - 팀 간 멤버 이동
+ * - 팀별 PM 자동 지정
+ * - PM 중복 지정 방지
+ * - 팀별 인원 밸런스 조정
+ * 
+ * 3. 예외 처리
+ * - 잘못된 입력값 처리
+ * - 빈 배열 처리
+ * - 최대 인원 초과 처리
+ * 
+ * 4. 기타 기능
+ * - 팀 이름 커스터마이징
+ * - 팀 통계 정보
+ * - 멤버 검색 기능
  */ 
