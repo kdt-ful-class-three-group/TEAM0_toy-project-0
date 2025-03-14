12388da5a3edf834df00ddf86f431988a18c3336
kongukjae <kongukjae@gmail.com>
2025-03-14 10:47:09 +0900

    demoMembers 데이터 가져오기: 팀 결정 테스트에서 demoMembers를 외부 데이터로 변경

--- model/team-decision-test.js
+++ model/team-decision-test.js
@@ -1,7 +1,5 @@
 import shuffleArray from "./modules/shffleArray.js";
-
-
-const demoMembers = [ "피카츄", "라이츄", "파이리", "꼬부기", "버터풀", "야도란", "피존투", "또가스", "단데기", "딱충이" ];
+import demoMembers from "../data/demoMembers.js";
 
 const temp = "";
 const blueTeam = [];
