9de6cdd5d2d05b01df5d0e1ae5c21d74008b947c
kongukjae <kongukjae@gmail.com>
2025-03-18 12:47:34 +0900

    코어 유틸리티 모듈 추가: 함수형 프로그래밍, 타입 관련, 불변성 관련 유틸리티를 내보내는 새로운 인덱스 파일을 생성하여 코드 구조를 개선하고 모듈화를 강화함.

---
+++ public/js/utils/core/index.js
@@ -0,0 +1,13 @@
+/**
+ * @file utils/core/index.js
+ * @description 코어 유틸리티 모듈 내보내기
+ */
+
+// 함수형 프로그래밍 유틸리티
+export * from './functional.js';
+
+// 타입 관련 유틸리티
+export * from './types.js';
+
+// 불변성 관련 유틸리티
+export * from './immutable.js'; 
